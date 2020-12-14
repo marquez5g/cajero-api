b0796c6803417db379813483e5c8fd5fb0575742
Gabriel Marquez <marquez122@outlook.com>
2020-12-14 08:19:18 -0500

    Eliminacion carpeta redundante

---
+++ cajero_api/db/db_connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:Trappist1!@localhost:5432/misiontic"
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base = declarative_base()
+Base.metadata.schema = "cajerodb"
